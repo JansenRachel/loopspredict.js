6acdf5979d32f7e0f35fd4e297af93805305ae4f
Rachel <rachel.jansen0613@gmail.com>
2021-05-04 14:33:56 -0500

    Solved prediction of all 3 loops

--- loopspredict.js
+++ loopspredict.js
@@ -3,6 +3,12 @@ for(var num = 0; num < 15; num++){
     console.log(num);
 }
 
+Variable    Value
+  num         0->1->2->3->4->5->6->7->8->9->10->11->12->13->14
+
+console.log
+    1,2,3,4,5,6,7,8,9,10,11,12,13,14
+
 
 
 
@@ -12,6 +18,12 @@ for(var i = 1; i < 10; i+=2){
         console.log(i);
     }
 }
+
+Variable    Value
+    i         1-> 3-> 5-> 7-> 9-> 11
+
+console.log
+    3,9
 
 
 
@@ -26,3 +38,9 @@ for(var j = 1; j <= 15; j++){
     }
     console.log(j);
 }
+
+Variable    Value
+    j         1-> 2-> 4-> 5-> 6-> 8-> 9-> 10-> 11-> 12-> 14-> 15, 16
+
+console.log
+    1,4,5,8,10,11,14,16
